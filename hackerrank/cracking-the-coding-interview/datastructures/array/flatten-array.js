b3dd2d5118fd5aa23ee5c88b022a2172fd973040
restrry <restrry@gmail.com>
2017-11-14 09:48:41 +0100

    flatten array

--- hackerrank/cracking-the-coding-interview/datastructures/array/flatten-array.js
+++ hackerrank/cracking-the-coding-interview/datastructures/array/flatten-array.js
@@ -1,19 +1,43 @@
 const { expect } = require('chai');
 
-function flatten(...args){
-    return args.reduce((acc, x) => {
-            return [].concat(acc, Array.isArray(x) ? flatten(...x) : x);
-    }, []);
-};
+const flatten = (input) =>
+    input.reduce((acc, x) =>
+        [].concat(acc, Array.isArray(x) ? flatten(x) : x),
+        []
+    );
 
 
-const input1 = [1, [2, 3], 4, 5, [6, [7]]];
+const input1 = [[[1]], [2, 3], 4, 5, [6, [7]]];
 const output1 = [1, 2, 3, 4, 5, 6, 7];
 
-expect(flatten(...input1)).to.deep.equal(output1);
+expect(flatten(input1)).to.deep.equal(output1);
 
 
 const input2 = ['a', ['b', 2], 3, null, [[4], ['c']]];
 const output2 = ['a', 'b', 2, 3, null, 4, 'c'];
 
-expect(flatten(...input2)).to.deep.equal(output2);
+expect(flatten(input2)).to.deep.equal(output2);
+
+const forEachRight = (arr, fn) => {
+    let i = arr.length - 1;
+    while(i >= 0){
+        fn(arr[i]);
+        i -= 1;
+    }
+}
+const flatten2 = (input) => {
+    const queue = [input];
+    const result = [];
+    while(queue.length){
+        const head = queue.shift();
+        if(Array.isArray(head)){
+            forEachRight(head, i => queue.unshift(i));
+        } else {
+            result.push(head);
+        }
+    }
+    return result;
+}
+
+expect(flatten2(input1)).to.deep.equal(output1);
+expect(flatten2(input2)).to.deep.equal(output2);
